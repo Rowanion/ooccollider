7044befa911a1989a83b6595464fe59a495297c2
weltmarktfuehrer <weltmarktfuehrer@09446a8e-1ed9-11de-be07-9135ae5da72a>
2009-04-14 15:24:18 +0000

    Octree code-capsules

---
+++ OOCTools/src/OctreeNode.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file	OctreeNode.cpp
+ * @author	TheAvatar <weltmarktfuehrer@googlemail.com>
+ * @version 1.0
+ * @date 	Created on: 13.04.2009
+ * 
+ */
+
+#include "OctreeNode.h"
+
+namespace ooctools {
+
+OctreeNode::OctreeNode()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+OctreeNode::~OctreeNode()
+{
+	// TODO Auto-generated destructor stub
+}
+
+}
