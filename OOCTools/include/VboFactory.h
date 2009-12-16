76a44250c766d4dea7b70e94b700f8efd5f894ff
weltmarktfuehrer <weltmarktfuehrer@09446a8e-1ed9-11de-be07-9135ae5da72a>
2009-12-16 18:55:24 +0000

    Started implementation of own memory management - <ger>krasse scheisse!</ger>

--- OOCTools/include/VboFactory.h
+++ OOCTools/include/VboFactory.h
@@ -10,23 +10,43 @@
 #define VBOFACTORY_H_
 
 #include "IndexedVbo.h"
+
 #include <boost/system/config.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
 
+#include <set>
+
 namespace ooctools {
 class VboFactory;
 
+struct Memory{
+	Memory(unsigned _size, uint64_t* _addr);
+	unsigned size;
+	uint64_t* address;
+	bool operator<(const Memory& _rhs) const;
+};
+
+typedef std::set<Memory>::iterator MemIt;
+typedef std::set<Memory>::const_iterator MemCIt;
+typedef std::set<Memory>::reverse_iterator MemRIt;
+
 struct IVbo{
-	friend class VboFactory;
+//	friend class VboFactory;
 	static uint64_t getAddi();
 	void debug();
 	unsigned* indexData();
 	V4N4* vertexData();
-private:
+	uint64_t getId();
+	unsigned getIndexCount();
+	unsigned getVertexCount();
+	unsigned getByteSize();
+
+//private:
 	uint64_t mPriId;
 	unsigned mPriIndexCount;
 	unsigned mPriVertexCount;
+	IVbo** mPriRegistryPtr;
 	unsigned mPriByteSize;
 	char mPriIsOffline;
 	char mPriFree;
@@ -36,13 +56,21 @@ class VboFactory {
 public:
 	virtual ~VboFactory();
 	IVbo* newVbo(boost::filesystem::ifstream* _inFile, unsigned _pos);
+	MemIt findFirstFit(const unsigned _byteSize);
 
-	static VboFactory *getSingleton();
+	static VboFactory* getSingleton();
+	IVbo** getNewRegistryPtr();
+	IVbo** insertIntoRegistry(IVbo* _iVbo);
 
 private:
 	VboFactory();
 	static VboFactory* mPriInstancePtr;
-	char* mPriMem;
+	uint64_t* mPriMem;
+	std::set<Memory> mPriFreeMap;
+	std::set<IVbo*> mPriIVboMap;
+
+	std::set<IVbo**> mPriIVboRegistry;
+
 };
 
 }
--- OOCTools/src/VboFactory.cpp
+++ OOCTools/src/VboFactory.cpp
@@ -11,6 +11,8 @@
 #include <iostream>
 #include <cstdlib>
 
+#define MEM_BLOCK_SIZE 1024*1024*8
+
 using namespace std;
 namespace fs=boost::filesystem;
 
@@ -29,10 +31,13 @@ VboFactory* VboFactory::getSingleton()
 
 VboFactory::VboFactory() :	mPriMem(0)
 {
-	mPriMem = new char[1024*1024*64];
-	cerr << "sizeof iVBO: " << sizeof(IndexedVbo)<< endl;
+	mPriMem = new uint64_t[MEM_BLOCK_SIZE];
+	mPriFreeMap.insert(Memory(MEM_BLOCK_SIZE*sizeof(uint64_t), mPriMem));
+	cerr << "sizeof iVBO: " << sizeof(IVbo)<< endl;
 
+	cerr << "first addr: " << (uint64_t)mPriMem << endl;
 	cerr << "sum: " << sizeof(V4N4*)+sizeof(unsigned)+sizeof(unsigned*)+sizeof(unsigned)+sizeof(bool)+(sizeof(GLuint)*2)+sizeof(bool)+sizeof(uint64_t)+sizeof(fs::ifstream*)+sizeof(unsigned) << endl;
+
 	cerr << "sizeof bool " << sizeof(bool) << endl;
 
 }
@@ -41,12 +46,25 @@ VboFactory::~VboFactory() {
 	delete[] mPriMem;
 }
 
+MemIt VboFactory::findFirstFit(const unsigned _byteSize)
+{
+	set<Memory>::iterator mIt = mPriFreeMap.begin();
+	for (; mIt != mPriFreeMap.end(); ++mIt){
+		if (mIt->size >= _byteSize){
+			return mIt;
+		}
+	}
+	return mPriFreeMap.end();
+}
+
+/**
+ * @warning Beware, he, who treads lightly in those dangerous and uncharted waters!
+ */
 IVbo* VboFactory::newVbo(boost::filesystem::ifstream* _inFile, unsigned _pos)
 {
-	//TODO beware, he, who treads lightly in those uncharted waters!
-	char* memPos = mPriMem;
 
-	unsigned tmpId, tmpIdxCount, tmpVertCount, tmpSize;
+	uint64_t tmpId;
+	unsigned tmpIdxCount, tmpVertCount, tmpSize;
 
 	_inFile->seekg(_pos, ios::beg);
 	_inFile->read((char*)&tmpId, sizeof(uint64_t));
@@ -55,29 +73,61 @@ IVbo* VboFactory::newVbo(boost::filesystem::ifstream* _inFile, unsigned _pos)
 	_inFile->seekg(_pos+sizeof(uint64_t)+sizeof(unsigned), ios::beg);
 	_inFile->read((char*)&tmpVertCount, sizeof(unsigned));
 	_inFile->seekg(_pos+sizeof(uint64_t)+(sizeof(unsigned)*2), ios::beg);
-	tmpSize = sizeof(uint64_t) + sizeof(unsigned)*3 + sizeof(bool)*2 + sizeof(unsigned)*tmpIdxCount + sizeof(V4N4)*tmpVertCount;
+	tmpSize = sizeof(IVbo) + sizeof(unsigned)*tmpIdxCount + sizeof(V4N4)*tmpVertCount;
 
 	//TODO find fitting memory block, repack otherwise
-	*((uint64_t*)memPos) = tmpId;
-	*((unsigned*)(memPos+sizeof(uint64_t))) = tmpIdxCount;
-	*((unsigned*)(memPos+sizeof(uint64_t) + sizeof(unsigned))) = tmpVertCount;
-	*((unsigned*)(memPos+sizeof(uint64_t) + sizeof(unsigned)*2)) = tmpSize;
-
-	((IVbo*)memPos)->debug();
-	((IVbo*)memPos)->indexData();
-	cerr << "mempos " << (uint64_t)memPos << endl;
+	// ------------------------
+	char* memPos;
+	MemIt it = findFirstFit(tmpSize);
+	if (it == mPriFreeMap.end()){
+		//TODO resize/decache
+		cerr << "last size: " << mPriFreeMap.rbegin()->size << endl;
+		cerr << "last addr: " << (uint64_t)mPriFreeMap.rbegin()->address << endl;
+		return 0;
+	}
+	else { // found a matching piece. Now get new free ptr
+		uint64_t* newAddr = ((uint64_t*)(((char*)it->address)+tmpSize));
+		Memory m = Memory(it->size-tmpSize, newAddr);
+		memPos = (char*)it->address;
+		//TODO this needs to be verified. <underscore>Seriously</underscore>!
+		*getNewRegistryPtr() = (IVbo*)memPos;
+		cerr << "mapsize b4: " << mPriFreeMap.size() << ", " << mPriFreeMap.begin()->size << endl;
+		mPriFreeMap.erase(it);
+		mPriFreeMap.insert(m);
+//		cerr << "currentPTR: " << (uint64_t)m.address << endl;
+//		cerr << "oldPTR: " << (uint64_t)memPos << endl;
+		cerr << "mapsize aft: " << mPriFreeMap.size() << ", " << mPriFreeMap.begin()->size << endl;
+	}
+	// ------------------------
+	((IVbo*)memPos)->mPriId = tmpId;
+	((IVbo*)memPos)->mPriIndexCount = tmpIdxCount;
+	((IVbo*)memPos)->mPriVertexCount = tmpVertCount;
+	((IVbo*)memPos)->mPriByteSize = tmpSize;
 
 	_inFile->read((char*)((IVbo*)memPos)->indexData(), sizeof(unsigned)*tmpIdxCount);
 	_inFile->seekg(_pos+sizeof(uint64_t)+(sizeof(unsigned)*2) + (sizeof(unsigned)*tmpIdxCount), ios::beg);
 
 	_inFile->read((char*)((IVbo*)memPos)->vertexData(), sizeof(V4N4)*tmpVertCount);
-//	_inFile->seekg(mPriFPos+sizeof(uint64_t)+(sizeof(unsigned)*2) + (sizeof(unsigned)*mPriIndexCount + (sizeof(V4N4)*mPriVertexCount)), ios::beg);
-//	mPriFPos += sizeof(uint64_t)+(sizeof(unsigned)*2) + (sizeof(unsigned)*mPriIndexCount + (sizeof(V4N4)*mPriVertexCount));
-//	cerr << "id: " << tmpId<< endl;
-//	cerr << "idx: " << (((IVbo*)memPos)->indexData())[3] << ", " << (((IVbo*)memPos)->indexData())[4] << ", " << (((IVbo*)memPos)->indexData())[5] << endl;
-//	cerr << "vert: " << (((IVbo*)memPos)->vertexData())[0].v[0] << ", " << (((IVbo*)memPos)->vertexData())[0].v[1] << ", " << (((IVbo*)memPos)->vertexData())[0].v[2] << endl;
+//	((IVbo*)memPos)->debug();
+	return (IVbo*)memPos;
+}
 
-	return 0;
+IVbo** VboFactory::getNewRegistryPtr()
+{
+	//TODO
+	pair<set<IVbo**>::iterator,bool> result;
+	result = mPriIVboRegistry.insert(new IVbo*());
+	return (*result.first);
+//	return 0;
+}
+
+IVbo** VboFactory::insertIntoRegistry(IVbo* _iVbo)
+{
+	//TODO
+	pair<set<IVbo**>::iterator,bool> result;
+	result = mPriIVboRegistry.insert(new IVbo*());
+	*(*result.first) = _iVbo;
+	return (*result.first);
 }
 
 void IVbo::debug()
@@ -86,18 +136,51 @@ void IVbo::debug()
 	cerr << "iIdxc " << mPriIndexCount << " - " << ((uint64_t)&mPriIndexCount) - ((uint64_t)this) << endl;
 	cerr << "iVertc " << mPriVertexCount << " - " << ((uint64_t)&mPriVertexCount) - ((uint64_t)this) << endl;
 	cerr << "iSize " << mPriByteSize << " - " << ((uint64_t)&mPriByteSize) - ((uint64_t)this) << endl;
-	cerr << "iIdx " << indexData()[0] << " - " << ((uint64_t)indexData())-((uint64_t)this) << endl;
-	cerr << "iVert " << vertexData()[0].v[0] << " - " << ((uint64_t)vertexData())-((uint64_t)this) << endl;
+	cerr << "iIdx " << " - " << (uint64_t)(indexData())-((uint64_t)this) << endl;
+	cerr << "iVert " << " - " << ((uint64_t)vertexData())-((uint64_t)this) << endl;
+	cerr << "indices " << indexData()[0] << ", " << indexData()[1] << ", " << indexData()[2] << endl;
+	cerr << "vertices " << vertexData()[0].v[0] << ", " << vertexData()[0].v[1] << ", " << vertexData()[0].v[2] << endl;
 }
 
 unsigned* IVbo::indexData()
 {
-	return (unsigned*)(this + sizeof(uint64_t) + (3*sizeof(unsigned)) + sizeof(char)*2);
+	return (unsigned*)(this+1);
 }
 
 V4N4* IVbo::vertexData()
 {
-	return (V4N4*)(this + sizeof(uint64_t) + (3*sizeof(unsigned)) + (mPriIndexCount*sizeof(unsigned)));
+	return (V4N4*) (((char*)(this+1)) + (mPriIndexCount*sizeof(unsigned)));
+}
+
+uint64_t IVbo::getId()
+{
+	return mPriId;
+}
+
+unsigned IVbo::getIndexCount()
+{
+	return mPriIndexCount;
+}
+
+unsigned IVbo::getVertexCount()
+{
+	return mPriVertexCount;
+}
+
+unsigned IVbo::getByteSize()
+{
+	return mPriByteSize;
+}
+
+Memory::Memory(unsigned _size, uint64_t* _addr) :
+	size(_size), address(_addr)
+{
+
+}
+
+bool Memory::operator<(const Memory& _rhs) const
+{
+	return (address < _rhs.address);
 }
 
 }
