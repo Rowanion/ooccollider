5a06cc0bcb829188ff109065fe8a0edd540c7fee
weltmarktfuehrer <weltmarktfuehrer@09446a8e-1ed9-11de-be07-9135ae5da72a>
2010-07-15 17:02:26 +0000

    Further Cleanup

--- trunk/RenderingSys/include/RsCGShaderBuilder.h
+++ trunk/RenderingSys/include/RsCGShaderBuilder.h
@@ -21,36 +21,108 @@ public:
         GeometryShader = 1,
         FragmentShader = 2};
 
-    static unsigned BuildShader(const char* _src, CGShaderType _type);
-    static unsigned BuildShader(const char* _vs, const char* _fs);
-    static unsigned BuildShader(const char* _vs, const char* _gs, const char* _fs);
+    static RsCGShaderBuilder* getSingleton();
 
-    static CGprogram BuildShaderFromFile(const std::string &_file, const char* _main, RsCGShaderBuilder::CGShaderType _type);
-    static CGprogram BuildShaderFromFile(const std::string &_vsfile, const char* _vsMain, const std::string &_fsfile, const char* _fsMain);
-    static CGprogram BuildShaderFromFile(const std::string &_vsfile, const std::string &_gsfile, const std::string &_fsfile);
-    static CGprogram BuildShaderFromFile(const std::string &_file, const char* _vsMain, const char* _gsMain, const char* _fsMain);
+    /**
+     * @brief one src string - up to three programs
+     * @param _src
+     * @param _vsMain
+     * @param _gsMain
+     * @param _fsMain
+     * @return
+     */
+    CGprogram BuildShader(const char* _src, const char* _vsMain, const char* _gsMain, const char* _fsMain);
 
-    static void EnableShader(CGprogram _shader);
-    static void DisableShader(CGprogram _shader);
-    static const char* IsCGError();
-    static void VerboseCompileOutput(CGprogram _shader, CGprofile _profile, const std::string& _file);
-    static void PrintShadingLanguageString();
+    /**
+     * @brief One source string - one particular program
+     * @param _src
+     * @param _main
+     * @param _type
+     * @return
+     */
+    CGprogram BuildShader(const char* _src, const char* _main, RsCGShaderBuilder::CGShaderType _type);
+
+    /**
+     * @brief One file - one particular program
+     * @param _file
+     * @param _main
+     * @param _type
+     * @return
+     */
+    CGprogram BuildShaderFromFile(const std::string &_file, const char* _main, RsCGShaderBuilder::CGShaderType _type);
+
+    /**
+     * @brief Two files - for exactly two programs: a vertex- and a fragment shader.
+     * @param _vsfile
+     * @param _vsMain
+     * @param _fsfile
+     * @param _fsMain
+     * @return
+     */
+    CGprogram BuildShaderFromFile(const std::string &_vsfile, const char* _vsMain, const std::string &_fsfile, const char* _fsMain);
+
+    /**
+     * @brief One file - up to three programs
+     * @param _file
+     * @param _vsMain
+     * @param _gsMain
+     * @param _fsMain
+     * @return
+     */
+    CGprogram BuildShaderFromFile(const std::string &_file, const char* _vsMain, const char* _gsMain, const char* _fsMain);
+
+    /**
+     * @brief Three compiled object files - exactly three programs
+     * @param _file
+     * @param _vsMain
+     * @param _gsMain
+     * @param _fsMain
+     * @return
+     */
+    CGprogram BuildShaderFromObjectFile(const std::string &_vsFile, const char* _vsMain, const std::string &_gsFile, const char* _gsMain, const std::string &_fsFile, const char* _fsMain);
+
+    /**
+     * @brief One compiled object file - one particular program
+     * @param _file
+     * @param _main
+     * @param _type
+     * @return
+     */
+    CGprogram BuildShaderFromObjectFile(const std::string &_file, const char* _main, RsCGShaderBuilder::CGShaderType _type);
+    void EnableShader(CGprogram _shader);
+    void DisableShader(CGprogram _shader);
+
+    /**
+     * @brief Checks whether or not a CGerror happened.
+     * @return the error-string if a CGerror occurred - 0 otherwise.
+     */
+    const char* IsCGError();
+
+    /**
+     * @brief Send some more information to std::cerr when a compile error occured.
+     * @param _shader The CGprogram for which the error occured
+     * @param _profile The CGprofile for which the error occured
+     * @param _code The source code of the erroneous program or, at least, the its filename.
+     */
+    void VerboseCompileOutput(CGprogram _shader, CGprofile _profile, const char* _data);
+    void PrintShadingLanguageString();
+    CGprogram combineAndLoad(CGprogram* _programs, int _pCount);
 
 private:
     RsCGShaderBuilder();
     virtual ~RsCGShaderBuilder();
 
-    static bool priPrintInfoLog(GLhandleARB _handle, const char* _shader);
-    static char* priReadShaderSource(const char *_filename);
-    static std::string priLanguageVersion;
-    static bool priInitialized;
-    static CGcontext priContext;
-	static CGprofile priProfiles[3];
-	static CGbool priHasVertexShaderSuport;
-	static CGbool priHasGeometryShaderSuport;
-	static CGbool priHasFragmentShaderSuport;
+    bool priPrintInfoLog(GLhandleARB _handle, const char* _shader);
+    char* priReadShaderSource(const char *_filename);
+    std::string priLanguageVersion;
+    bool priInitialized;
+    CGcontext priContext;
+	CGprofile priProfiles[3];
+	CGbool priHasVertexShaderSuport;
+	CGbool priHasGeometryShaderSuport;
+	CGbool priHasFragmentShaderSuport;
 
-    static void priInit();
+    void priInit();
 };
 
 #endif	/* RSCGSHADERBUILDER_H */
